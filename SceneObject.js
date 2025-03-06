b9db83306067c55d916fc9eeb7368aacaacaf1f2
KokkonenA <kokkonena550@gmail.com>
2025-03-06 21:08:59 +0200

    Display the crime scene.

---
+++ SceneObject.js
@@ -0,0 +1,20 @@
+class SceneObject {
+  constructor(img, x, y, scale) {
+    img.resize(scale*img.width, scale*img.height)
+    this.img = img;
+    this.x = x;
+    this.y = y;
+    this.scale = scale;
+    this.children = [];
+  }
+
+  addChild(img, x, y, scale)
+  {
+    this.children.push(new SceneObject(img, this.scale*x + this.x, this.scale*y + this.y, this.scale*scale));
+  }
+  
+  display() {
+    image(this.img, this.x, this.y);
+    this.children.forEach(child => child.display());
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,8 +1,57 @@
+function preload() {
+  backgroundImg = loadImage('assets/walls.png');
+  cakeImg =  loadImage('assets/cake.png');
+  cdPlayerImg = loadImage('assets/cd_player.png');
+  cigarettesImg = loadImage('assets/cigarettes.png');
+  dogFoodImg = loadImage('assets/dog_food.png');
+  condomImg = loadImage('assets/durex_condom.png');
+  letterImg = loadImage('assets/letter.png');
+  mrMomoImg = loadImage('assets/mr.momo.png');
+  oldTvImg = loadImage('assets/old_tv.png');
+  orangeImg = loadImage('assets/orange.png');
+  pizzaBoxImg = loadImage('assets/pizza_box.png');
+  rUOkImg = loadImage('assets/r_u_ok.png');
+  rugImg = loadImage('assets/rug.png');
+  tableImg = loadImage('assets/table.png');
+  teaMugImg = loadImage('assets/tea_mug.png');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  windowImageWidthRelation = windowWidth / backgroundImg.width;
+  windowImageHeightRelation = windowHeight / backgroundImg.height;
+
+  if (windowImageWidthRelation < windowImageHeightRelation) {
+    backgroundScale = windowImageWidthRelation;
+    canvasX = 0;
+    canvasY = (windowHeight - backgroundScale * backgroundImg.height) / 2;
+  }
+  else {
+    backgroundScale = windowImageHeightRelation;
+    canvasX = (windowWidth - backgroundScale * backgroundImg.width) / 2;
+    canvasY = 0;
+  }
+
+  canvas = createCanvas(backgroundScale*backgroundImg.width, backgroundScale*backgroundImg.height);
+  canvas.position(canvasX, canvasY);
+
+  // to do: more sophisticated layering?
+  crimeScene = new SceneObject(backgroundImg, 0, 0, backgroundScale);
+  crimeScene.addChild(cakeImg, 1500, 1100, 1);
+  crimeScene.addChild(rugImg, 2900, 1000, 1);
+  crimeScene.addChild(cigarettesImg, 3200, 1400, 1);
+  crimeScene.addChild(rUOkImg, 3380, 1600, 1);
+  crimeScene.addChild(dogFoodImg, 3200, 1600, 1);
+  crimeScene.addChild(condomImg, 3300, 2000, 1);
+  crimeScene.addChild(mrMomoImg, 1500, 1600, 1);
+  crimeScene.addChild(oldTvImg, 900, 500, 1);
+  crimeScene.addChild(orangeImg, 500, 1900, 1);
+  crimeScene.addChild(pizzaBoxImg, 3300, 700, 1);
+  crimeScene.addChild(letterImg, 2700, 1000, 1);
+  crimeScene.addChild(tableImg, 2200, 700, 1);
+  crimeScene.addChild(cdPlayerImg, 2300, 800, 1);
+  crimeScene.addChild(teaMugImg, 1800, 1000, 1);
 }
 
 function draw() {
-  background(220);
-  text("Hello World", 200, 200);
+  crimeScene.display();
 }
